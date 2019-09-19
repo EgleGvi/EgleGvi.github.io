df284f9c6aa7b04553ca17d35adf9e508c8025ca
EgleGvi <e.gvildyte@gmail.com>
2019-09-19 18:43:20 +0300

    begin work on dynamic content loading

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-localStorageDebug();
-//window.localStorage.clear();
+$(document).ready(function(){
+    loadContent("admin.html");
+    loadSpecialistsInToAdminPage();
+    localStorageDebug();
+    //window.localStorage.clear();
+});
+
+function loadSpecialistsInToAdminPage () {
+    var specialists = readFromLocalStorage(LOCAL_STORAGE_SPECIALISTS);
+    if(specialists === []) {
+        return;
+    }
+    var html = "";
+    specialists.forEach(function (specialist) {
+        html+='<option value="'+specialist+'">'+specialist+'</option>';
+    });
+    $('#service').append(html);
+}
 
 $('#queue-form').submit(function (event) {
     event.preventDefault();
@@ -7,3 +23,40 @@ $('#queue-form').submit(function (event) {
     var queueNumber = $('#queue-number').val();
     addToQueue(service, queueNumber);
 });
+
+function loadFile() {
+    var input, file, fr;
+
+    if (typeof window.FileReader !== 'function') {
+        alert("The file API isn't supported on this browser yet.");
+        return;
+    }
+
+    input = document.getElementById('fileinput');
+    if (!input) {
+        alert("Um, couldn't find the fileinput element.");
+    }
+    else if (!input.files) {
+        alert("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+        alert("Please select a file before clicking 'Load'");
+    }
+    else {
+        file = input.files[0];
+        fr = new FileReader();
+        fr.onload = receivedText;
+        fr.readAsText(file);
+    }
+
+    function receivedText(e) {
+        let lines = e.target.result;
+        var data = JSON.parse(lines);
+        storeSpecialists(data.specialists);
+        loadSpecialistsInToAdminPage();
+    }
+}
+
+function loadContent(htmlFile) {
+    $(".container").load(htmlFile);
+}
--- js/localStorageHelper.js
+++ js/localStorageHelper.js
@@ -1,23 +1,31 @@
-var LOCAL_STORAGE_QUEUE_NAME = 'clientQueue';
+var LOCAL_STORAGE_QUEUE = 'clientQueue';
+var LOCAL_STORAGE_SPECIALISTS = 'specialists';
 
-function getQueue (){
-    var queueData = window.localStorage.getItem(LOCAL_STORAGE_QUEUE_NAME);
-    if(queueData == null) {
+function readFromLocalStorage (key){
+    var data = window.localStorage.getItem(key);
+    if(data == null) {
         return [];
     }
-    return JSON.parse(queueData);
+    return JSON.parse(data);
 }
 
 function addToQueue (service, queueNumber) {
-    var queueData = getQueue();
+    var queueData = readFromLocalStorage(LOCAL_STORAGE_QUEUE);
     var newItem = {
         'service': service,
         'queueNumber': queueNumber
     };
     queueData.push(newItem);
-    window.localStorage.setItem(LOCAL_STORAGE_QUEUE_NAME, JSON.stringify(queueData));
+    window.localStorage.setItem(LOCAL_STORAGE_QUEUE, JSON.stringify(queueData));
 }
 
-function localStorageDebug() {
-    console.log(JSON.stringify(getQueue()));
+function storeSpecialists(specialists) {
+    window.localStorage.setItem(LOCAL_STORAGE_SPECIALISTS, JSON.stringify(specialists));
 }
+
+
+function localStorageDebug() {
+    console.log(JSON.stringify(readFromLocalStorage(LOCAL_STORAGE_QUEUE)));
+    console.log(JSON.stringify(readFromLocalStorage(LOCAL_STORAGE_SPECIALISTS)));
+}
+
